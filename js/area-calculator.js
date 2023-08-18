ea89c316e33c786fbf9e3def68a34bad47c2116f
Akash Khan <akashcc48@gmail.com>
2023-08-18 07:42:24 +0600

    code validate added

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -4,7 +4,15 @@ function calculateTriangleArea() {
 
   // Get triangle height value
   const height = getInputValue("triangle-height");
-
+  // Validate The Function
+  if (typeof base !== "number" || typeof height !== "number") {
+    alert("Please Type a valid number");
+    return;
+  } else if (isNaN(base) || isNaN(height)) {
+    alert("Invalid Input");
+    return;
+  }
+  // Calculate triangle area
   const area = 0.5 * base * height;
   setElementInnerText("triangle-area", area);
 }
@@ -14,6 +22,15 @@ function calculateRectangleArea() {
   const width = getInputValue("rectangle-width");
   // Get rectangle length
   const length = getInputValue("rectangle-length");
+  // Validate The Function
+  if (typeof base !== "number" || typeof height !== "number") {
+    alert("Please Type a valid number");
+    return;
+  } else if (isNaN(base) || isNaN(height)) {
+    alert("Invalid Input");
+
+    return;
+  }
   // Calculate rectangle area
   const area = width * length;
   // show rectangle area
@@ -26,10 +43,32 @@ function calculateParallelogramArea() {
   const base = getInputValue("parallelogram-base");
   // Perallelogram height
   const height = getInputValue("parallelogram-height");
+  // Validate The Function
+  if (typeof base !== "number" || typeof height !== "number") {
+    alert("Please Type a valid number");
+    return;
+  } else if (isNaN(base) || isNaN(height)) {
+    alert("Invalid Input");
+    return;
+  }
   // Calculate Perallelogram area
   const area = height * base;
   // Show Perallelogram area
 
   setElementInnerText("parallelogram-area", area);
 }
-
+function calculateEllipseArea() {
+  const major = getInputValue("ellipse-major");
+  const minor = getInputValue("ellipse-minor");
+  // Validate The Function
+  if (typeof base !== "number" || typeof height !== "number") {
+    alert("Please Type a valid number");
+    return;
+  } else if (isNaN(base) || isNaN(height)) {
+    alert("Invalid Input");
+    return;
+  }
+  const area = 3.14 * major * minor;
+  const areaTowDecimal = area.toFixed(2);
+  setElementInnerText("ellipse-area", areaTowDecimal);
+}
--- js/function.js
+++ js/function.js
@@ -1,11 +1,4 @@
-function calculateEllipseArea() {
-  const major = getInputValue("ellipse-major");
-  const minor = getInputValue("ellipse-minor");
 
-  const area = 3.14 * major * minor;
-  const areaTowDecimal = area.toFixed(2);
-  setElementInnerText("ellipse-area", areaTowDecimal);
-}
 function getInputValue(inputId) {
   const inputFeild = document.getElementById(inputId);
   const inputValueText = inputFeild.value;
